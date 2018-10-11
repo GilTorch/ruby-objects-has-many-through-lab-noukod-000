65c60897019591eb2c2ca6b67a66ad810801d374
Gilbert Torchon <gilbert@noukod.com>
2018-10-11 19:29:13 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -5,5 +5,11 @@ class Patient
   def initialize(name)
     @name=name
   end
+
+  def new_appointment(doctor,date)
+     appointment = Appointment.new(doctor,self,date)
+     @appointments << appointment
+     appointment
+  end
   
 end
